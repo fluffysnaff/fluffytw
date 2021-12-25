73593ab7ccc3bad80555e24275025380f3c27531
fluffysnaff <fluffysnaff@protonmail.com>
2021-12-25 01:31:19 +0100

    Small refactor to aimbot_scans.cpp

--- aimbot/aimbot_scans.cpp
+++ aimbot/aimbot_scans.cpp
@@ -4,15 +4,14 @@
 vec2 FAimbot::HitpointScan(int id)
 {
 	const vec2 myPos = m_pClient->m_PredictedChar.m_Pos;
+	const vec2 myVel = m_pClient->m_PredictedChar.m_Vel;
 	const vec2 targetVel = m_pClient->m_aClients[id].m_Predicted.m_Vel;
 	vec2 targetPos = m_pClient->m_aClients[id].m_Predicted.m_Pos;
 
-	const vec2 scanDir = targetPos - myPos;
-
-	if(fHelper->HitScanHook(myPos, scanDir, PHYS_SIZE))
+	fHelper->PredictHook(myPos, myVel, targetPos, targetVel);
+	if(fHelper->HitScanHook(myPos, targetPos, PHYS_SIZE))
 	{
-		fHelper->PredictHook(myPos, targetPos, targetVel);
-		return targetPos - myPos;
+		return targetPos;
 	}
 	return vec2(0, 0);
 }
