b3d92bdbec851b727b2224bc0cd61bf3100f825c
fluffysnaff <fluffysnaff@protonmail.com>
2022-03-27 20:44:37 +0200

    Refactored configs to be global

--- f_config.h
+++ f_config.h
@@ -1,27 +1,28 @@
 #pragma once
 
-// TODO: Better configs
-
-namespace FConfig
+class FConfig
 {
-	struct EspConfig
+public:
+	struct AimbotCfg
 	{
-		bool fovEnabled;
-		int aimbotFov;
-	};
+		bool enabled = false;
+		float fov = 0.f;
+		bool silent = false;
+		bool hookVisible = false;
+		bool edge = false;
+		int scanType = 0;
+		float accuracy = 0.f;
+	} aimbotCfg;
 
-	struct AimbotConfig
+	struct EspCfg
 	{
-		bool enabled;
-		bool silent;
-		int fov;
-		bool edge;
-		bool accuracy;
-		bool hookVisible;
-		bool hook;
-		bool hammer;
-		bool grenade;
-		bool laser;
-		bool shotgun;
-	};
-}
+		bool enabled = false;
+		bool drawFov = false;
+	} espCfg;
+};
+
+extern std::unique_ptr<FConfig> fConfig;
+
+
+// Macros
+#define MAX_HITPOINTS 32
